fbe34de32652604606d7f6c7349ef501244055f6
Scott Morrison <scott@tqft.net>
2013-03-29 15:07:25 +1100

    jumbled journal search

--- src/test/scala/net/tqft/mathscinet/SearchApp.scala
+++ src/test/scala/net/tqft/mathscinet/SearchApp.scala
@@ -11,16 +11,20 @@ object SearchApp extends App {
     Eigenfactor.topJournals.take(20).iterator.map(Search.inJournal).flexibleTranspose.flatMap({ x => x })
   }
 
-  for (article <- Search.by("Izumi")) {
-    println(article.bibtex.toBIBTEXString)
-  }
-  
-//    for (article <- Search.everything) {
-//      println(article.identifierString + " ---> " + article.DOI)
-//    }
-
-//  for (article <- inTopJournals) {
+//  for (article <- Search.by("Izumi")) {
 //    println(article.bibtex.toBIBTEXString)
 //  }
+
+  //    for (article <- Search.everything) {
+  //      println(article.identifierString + " ---> " + article.DOI)
+  //    }
+
+  //  for (article <- inTopJournals) {
+  //    println(article.bibtex.toBIBTEXString)
+  //  }
+
+  for (article <- Search.inTopJournalsJumbled(20)) {
+    println(article.bibtex.toBIBTEXString)
+  }
 }
 
