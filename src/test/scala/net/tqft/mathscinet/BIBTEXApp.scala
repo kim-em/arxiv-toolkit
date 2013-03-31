f4d583a4ff953e828dd762ca825dc4071d2fd4de
Scott Morrison <scott@tqft.net>
2013-03-31 20:39:27 +1100

    disabling selenium while dumping bibtex

--- src/test/scala/net/tqft/mathscinet/BIBTEXApp.scala
+++ src/test/scala/net/tqft/mathscinet/BIBTEXApp.scala
@@ -1,9 +1,14 @@
 package net.tqft.mathscinet
 
+import scala.collection.parallel.ForkJoinTaskSupport
+import net.tqft.util.SeleniumSlurp
+
 object BIBTEXApp extends App {
-//	println(Article("MR1437496").bibtex)
-	
-	for(article <- Articles.withCachedBIBTEX) {
-	  println(article.identifierString + " ---> " + article.DOI)
-	}
+  SeleniumSlurp.disable
+
+  val pool = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(20))
+
+  for (group <- Articles.withCachedBIBTEX.grouped(1000); groupPar = { val p = group.par; p.tasksupport = pool; p }; article <- groupPar) {
+    println(article.bibtex.toBIBTEXString)
+  }
 }
