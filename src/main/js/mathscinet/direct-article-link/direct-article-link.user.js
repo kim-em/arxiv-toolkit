dfd6df94bb062f2d3525c5f5f147c9a45cae93e3
Scott Morrison <scott@tqft.net>
2013-04-16 21:55:19 +1000

    dropbox

--- src/main/js/mathscinet/direct-article-link/direct-article-link.user.js
+++ src/main/js/mathscinet/direct-article-link/direct-article-link.user.js
@@ -16,22 +16,20 @@ function main() {
     rewriteArticleLinks();
   });
 
-
-
 /* Hmm... my attemps to integrate with Dropbox seem to have run into a dead-end. The authenticate call never reaches the callback.
 There are some error messages on the console about permissions to "tabs" and "experimental.identity", which I haven't been able to resolve. */
-//    var client = new Dropbox.Client({ key: "cIrBuCz5CWA=|fGPZmdP8KEuRpnB0DUK27/oCcPvCWXzzJAF16wpHuA==" /* encoded at https://dl-web.dropbox.com/spa/pjlfdak1tmznswp/api_keys.js/public/index.html */, sandbox: true });
-//    client.authDriver(new Dropbox.Drivers.Chrome({ receiverPath: chrome.extension.getURL("oauth/chrome_oauth_receiver.html") }));
-//    client.authenticate(function(error, client) {
-//                        if (error) {
-//                            alert("Dropbox authentication failed: ", error);
-//                            // Don't forget to return from the callback, so you don't execute the code
-//                            // that assumes everything went well.
-//                            return false;
-//                        } else {
-//                            alert("Successfully authenticated Dropbox!");
-//                        }
-//                        });
+   var client = new Dropbox.Client({ key: "cIrBuCz5CWA=|fGPZmdP8KEuRpnB0DUK27/oCcPvCWXzzJAF16wpHuA==" /* encoded at https://dl-web.dropbox.com/spa/pjlfdak1tmznswp/api_keys.js/public/index.html */, sandbox: true });
+   client.authDriver(new Dropbox.Drivers.Chrome({ receiverPath: chrome.extension.getURL("oauth/chrome_oauth_receiver.html") }));
+   client.authenticate(function(error, client) {
+                       if (error) {
+                           alert("Dropbox authentication failed: ", error);
+                           // Don't forget to return from the callback, so you don't execute the code
+                           // that assumes everything went well.
+                           return false;
+                       } else {
+                           alert("Successfully authenticated Dropbox!");
+                       }
+                       });
 }
 
 function insertMenuItem() {
