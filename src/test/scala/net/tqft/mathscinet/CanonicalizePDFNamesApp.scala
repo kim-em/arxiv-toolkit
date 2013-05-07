6b54ca8fa9ee11af1aefdcbc9357ee9931ffeb94
Scott Morrison <scott@tqft.net>
2013-05-07 21:03:32 +1000

    fast

--- src/test/scala/net/tqft/mathscinet/CanonicalizePDFNamesApp.scala
+++ src/test/scala/net/tqft/mathscinet/CanonicalizePDFNamesApp.scala
@@ -13,34 +13,34 @@ object CanonicalizePDFNamesApp extends App {
 
   val identifierRegex = "MR[0-9]*".r
 
-//  val articles = (for (
-//    a <- Articles.fromBibtexGzipFile(System.getProperty("user.home") + "/projects/arxiv-toolkit/100_4.bib.gz");
-//    issn <- a.ISSNOption;
-//    if ISSNs.Elsevier.contains(issn)
-//  ) yield { a.identifierString -> a }).toMap
+  val articles = (for (
+    a <- Articles.fromBibtexGzipFile(System.getProperty("user.home") + "/projects/arxiv-toolkit/100_4.bib.gz");
+    issn <- a.ISSNOption;
+    if ISSNs.Elsevier.contains(issn)
+  ) yield { a.identifierString -> a }).toMap
 
 //   Verifying that we can build a name for *everything*
-    for (a <- Articles.fromBibtexGzipFile(System.getProperty("user.home") + "/projects/arxiv-toolkit/100_4.bib.gz")) {
-      try {
-        println(a.constructFilename())
-      } catch {
-        case e: Exception => {
-          e.printStackTrace()
-          println(a.bibtex.toBIBTEXString)
-          ???
-        }
-      }
-    }
-
-//  for (
-//    file <- directory.listFiles(new FilenameFilter { override def accept(dir: File, name: String) = identifierRegex.findFirstMatchIn(name).nonEmpty });
-//    identifier <- identifierRegex.findFirstIn(file.getName());
-//    article = articles.get(identifier).getOrElse(Article(identifier))
-//  ) {
-//    val newName = article.constructFilename()
-//    if (file.getName != newName) {
-//      println("Renaming " + file.getName + " to " + newName)
-//      file.renameTo(new File(directory, newName))
+//    for (a <- Articles.fromBibtexGzipFile(System.getProperty("user.home") + "/projects/arxiv-toolkit/100_4.bib.gz")) {
+//      try {
+//        println(a.constructFilename())
+//      } catch {
+//        case e: Exception => {
+//          e.printStackTrace()
+//          println(a.bibtex.toBIBTEXString)
+//          ???
+//        }
+//      }
 //    }
-//  }
+
+  for (
+    file <- directory.listFiles(new FilenameFilter { override def accept(dir: File, name: String) = identifierRegex.findFirstMatchIn(name).nonEmpty });
+    identifier <- identifierRegex.findFirstIn(file.getName());
+    article = articles.get(identifier).getOrElse(Article(identifier))
+  ) {
+    val newName = article.constructFilename()
+    if (file.getName != newName) {
+      println("Renaming " + file.getName + " to " + newName)
+      file.renameTo(new File(directory, newName))
+    }
+  }
 }
