8fd722cebffb415692076ed4015c58948a83684b
Scott Morrison <scott@tqft.net>
2013-08-14 16:44:55 +1000

    some k-theory related tweaks

--- src/test/scala/net/tqft/mathscinet/SearchApp.scala
+++ src/test/scala/net/tqft/mathscinet/SearchApp.scala
@@ -7,40 +7,44 @@ import net.tqft.journals.ISSNs
 
 object SearchApp extends App {
 
+  // K-Theory
+  for (article <- Search.inJournalsJumbled(Seq(ISSNs.`K-Theory`))) {
+    println(article.bibtex.toBIBTEXString)
+  }
+
   val `Advances, open access` = Search.query("arg3=&co4=AND&co5=NOT&co6=AND&co7=AND&dr=pubyear&extend=1&pg4=TI&pg5=JOUR&pg6=JOUR&pg7=ALLF&pg8=ET&review_format=html&s4=&s5=0001-8708&s6=Advancement&s7=&s8=All&vfpref=html&yearRangeFirst=&yearRangeSecond=2008&yrop=eq").toStream
-  for(a <- `Advances, open access`) {
+  for (a <- `Advances, open access`) {
     println(a.bibtex.toBIBTEXString)
   }
-  
+
   // Topology
   for (article <- Search.inJournalsJumbled(Seq(ISSNs.`Topology`))) {
     println(article.bibtex.toBIBTEXString)
   }
 
   //   Annals
-    for(article <- Search.inJournal("0003-486X")) {
-       println(article.bibtex.toBIBTEXString)   
-    }
+  for (article <- Search.inJournal("0003-486X")) {
+    println(article.bibtex.toBIBTEXString)
+  }
 
-    for (article <- Search.inTopJournalsJumbled(50)) {
-      println(article.bibtex.toBIBTEXString)
-    }
+  for (article <- Search.inTopJournalsJumbled(50)) {
+    println(article.bibtex.toBIBTEXString)
+  }
 
-    // Crelle's journal
-    for (article <- Search.inJournal("0075-4102")) {
-      println(article.bibtex.toBIBTEXString)
-    }
-  
-    // IMRN
-    for (article <- Search.inJournal("1073-7928")) {
-      println(article.bibtex.toBIBTEXString)
-    }
-  
-    // AMS
-    for (article <- Search.inJournalsJumbled(Seq("0002-9947", "1056-3911", "1073-7928", "0894-0347", "0075-4102", "1088-4173", "0273-0979", "0002-9939", "0025-5718", "0024-6107", "0002-9904", "0234-0852", "1088-4165", "0065-9266"))) {
-      println(article.bibtex.toBIBTEXString)
-    }
+  // Crelle's journal
+  for (article <- Search.inJournal("0075-4102")) {
+    println(article.bibtex.toBIBTEXString)
+  }
 
+  // IMRN
+  for (article <- Search.inJournal("1073-7928")) {
+    println(article.bibtex.toBIBTEXString)
+  }
+
+  // AMS
+  for (article <- Search.inJournalsJumbled(Seq("0002-9947", "1056-3911", "1073-7928", "0894-0347", "0075-4102", "1088-4173", "0273-0979", "0002-9939", "0025-5718", "0024-6107", "0002-9904", "0234-0852", "1088-4165", "0065-9266"))) {
+    println(article.bibtex.toBIBTEXString)
+  }
 
   // Elsevier
   for (article <- Search.inJournalsJumbled(ISSNs.Elsevier)) {
@@ -65,7 +69,6 @@ object SearchApp extends App {
   for (article <- Search.inTopJournalsJumbled(100)) {
     println(article.bibtex.toBIBTEXString)
   }
-
 
   FirefoxSlurp.quit
 }
