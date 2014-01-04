f3e41936b6dab34e28c586a44683111269ac499a
Scott Morrison <scott@tqft.net>
2014-01-04 19:59:01 +1100

    TOCBot

--- src/main/scala/net/tqft/mlp/TOCBot.scala
+++ src/main/scala/net/tqft/mlp/TOCBot.scala
@@ -18,22 +18,15 @@ object TOCBot extends App {
   }
 
   val journals = Eigenfactor.topJournals.take(1)
+//  val journals = Iterator(ISSNs.`Advances in Mathematics`, ISSNs.`Discrete Mathematics`, ISSNs.`Annals of Mathematics`, ISSNs.`Algebraic & Geometric Topology`, ISSNs.`Geometric and Functional Analysis`)
   val years = 2013 to 2013
 
   val articles = for (j <- journals; y <- years; a <- Search.inJournalYear(j, y)) yield a
 
-  //  def advancesArticles = for (a <- Search.inJournalYear(ISSNs.`Advances in Mathematics`, 2013); if a.journal != "Advancement in Math."; y <- a.yearOption; if y >= 2013) yield a
-  //  def annalsArticles = Search.inJournalYear(ISSNs.`Annals of Mathematics`, 2013)
-  //  def discreteMathArticles = Search.inJournalYear(ISSNs.`Discrete Mathematics`,2013)
-  //  def agtArticles = Search.inJournalYear(ISSNs.`Algebraic & Geometric Topology`, 2013)
-  //  def gafaArticles = Search.inJournalYear(ISSNs.`Geometric and Functional Analysis`,2013)
-  //
-  //  val articles = advancesArticles ++ annalsArticles ++ discreteMathArticles ++ agtArticles ++ gafaArticles
-
   val arranged = articles.toSeq.groupBy(_.journalOption).mapValues(_.groupBy(_.yearOption)).mapValues(_.mapValues(_.groupBy(_.volumeYearAndIssue)))
 
   def journalText(journal: String) = ""
-  def yearText(journal: String, year: Int) = "{{Data:" + journal + "/YearSummary/" + year + "}}\n\n"
+  def yearText(journal: String, year: Int) = "{{#ifexist:Data:" + journal + "/YearSummary/" + year + "|{{Data:" + journal + "/YearSummary/" + year + "}}}}\n\n"
 
   for ((Some(journal), years) <- arranged) {
     val j = pandoc.latexToText(journal)
