cbd6e8777aa2a0eb2644099936cf1b80ec8a80b9
Scott Morrison <scott@tqft.net>
2014-01-08 10:13:44 +1100

    minor

--- src/main/scala/net/tqft/mlp/DOIMatchBot.scala
+++ src/main/scala/net/tqft/mlp/DOIMatchBot.scala
@@ -3,10 +3,20 @@ package net.tqft.mlp
 import net.tqft.mlp.sql.SQLTables
 import scala.slick.driver.MySQLDriver.simple._
 import net.tqft.mlp.sql.SQL
+import net.tqft.mathscinet.Article
 
 object DOIMatchBot extends App {
 
   SQL { implicit session =>
+    val arxivDOIs = (for {
+      a <- SQLTables.arxiv;
+      if a.doi.isNotNull
+    } yield (a.doi)).take(5)
+    
+    println(arxivDOIs.run)
+    
+    println((for(a <- SQLTables.mathscinet; if a.MRNumber === 2453592) yield a).run)
+    
     val matchingDOIs = for {
       a <- SQLTables.arxiv;
       b <- SQLTables.mathscinet if a.doi === b.doi
@@ -23,11 +33,11 @@ object DOIMatchBot extends App {
 
     println("Matching DOIs:")
     for ((arxivid, mrnumber) <- matchingDOIs.run) {
-      println(arxivid + " " + mrnumber)
+      println(arxivid + " <---> " + Article(mrnumber).identifierString)
     }
-    println("Matching titles:")
-    for ((arxivid, mrnumber) <- matchingTitles.run) {
-      println(arxivid + " " + mrnumber)
-    }
+//    println("Matching titles:")
+//    for ((arxivid, mrnumber) <- matchingTitles.run) {
+//      println(arxivid + " " + mrnumber)
+//    }
   }
 }
