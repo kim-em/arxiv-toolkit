79b76f3fcecf304d3b0e11596112cd7ea3350769
Scott Morrison <scott@tqft.net>
2014-01-27 14:16:48 +1100

    bumping httpclient, finally

---
+++ src/main/scala/net/tqft/journals/Journals.scala
@@ -0,0 +1,18 @@
+package net.tqft.journals
+
+object ISSNs {
+  val Elsevier = Seq("0196-8858", "0001-8708", "0294-1449", "0003-4843", "0168-0072", "1063-5203", "0307-904X", "0893-9659", "0007-4497", "0925-7721", "0898-1221", "0926-2245", "0166-218X", "0012-365X", "1572-5286", "0195-6698", "0723-0869", "1071-5797", "0016-660X", "0315-0860", "0019-3577", "1385-7258", "0019-9958", "0890-5401", "0888-613X", "0021-7824", "0021-8693", "1570-8683", "0021-9045", "0021-9800", "0097-3165", "0095-8956", "0885-064X", "0377-0427", "0022-0000", "0022-0396", "1570-8667", "0022-1236", "0743-1066", "1567-8326", "0022-247X", "0047-259X", "0022-314X", "0022-4049", "0747-7171", "0024-3795", "0895-7177", "0270-0255", "0167-6423", "0304-4149", "0304-3975", "0040-9383", "0166-8641")
+  val `Topology` = "0040-9383"
+  val `Journal of Algebra` = "0021-8693"
+  val `Journal of Pure and Applied Algebra` = "0022-4049"
+  val `K-Theory` = "0920-3036"
+  val `Advances in Mathematics` = "0001-8708"
+  val `Discrete Mathematics` = "0012-365X"
+  val `Annals of Mathematics` = "0003-486X"
+  val `Algebraic & Geometric Topology` = "1472-2747"
+  val `Geometric and Functional Analysis` = "1016-443X"
+  val `Journal of Functional Analysis` = "0022-1236"
+  val `Journal of Number Theory` = "0022-314X"
+  val `Journal of Combinatorial Theory A` = "0097-3165"
+  val `Journal of Combinatorial Theory B` = "0095-8956"
+}
---
+++ src/test/scala/net/tqft/mathscinet/SQLImportApp.scala
@@ -0,0 +1,41 @@
+package net.tqft.mathscinet
+
+import net.tqft.mlp.sql.SQLTables
+import net.tqft.toolkit.Logging
+import net.tqft.toolkit.amazon.AnonymousS3
+import scala.collection.parallel.ForkJoinTaskSupport
+import net.tqft.mlp.sql.SQL
+
+object SQLImportApp extends App {
+
+  import scala.slick.driver.MySQLDriver.simple._
+
+  val pool = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(100))
+
+  val cache = AnonymousS3("LoM-bibtex")
+
+//  var maxlengths = IndexedSeq.fill(20)(0)
+//  var count = 0
+//
+//  for (group <- cache.keysIterator.grouped(1000); groupPar = { val p = group.par; p.tasksupport = pool; p }; k <- groupPar; a = Article(k)) {
+//    val lengths = for (i <- 2 until 22) yield a.sqlRow.productElement(i).asInstanceOf[Option[String]].getOrElse("").length()
+//    count += 1
+//    maxlengths = lengths.zip(maxlengths).map(p => scala.math.max(p._1, p._2))
+//    if(count % 1000 == 0) println(maxlengths)
+//  }
+
+  SQL { implicit session =>
+      for (group <- cache.keysIterator.toSeq.sorted.reverse.grouped(1000); groupPar = { val p = group.par; p.tasksupport = pool; p }; k <- groupPar; a = Article(k)) {
+        println("inserting: " + a.identifierString)
+        try {
+          SQLTables.mathscinet += a.sqlRow
+        } catch {
+          case e: com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException if e.getMessage().startsWith("Duplicate entry") => {}
+          case e: Exception => {
+            Logging.error("Exception while inserting \n" + a.bibtex.toBIBTEXString, e)
+//            throw e
+          }
+        }
+      }
+  }
+}
