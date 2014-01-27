9f9bb751025435bc56fa9bc581409d2952ff1252
Scott Morrison <scott@tqft.net>
2014-01-27 14:26:22 +1100

    minor

--- src/main/scala/net/tqft/mlp/package.scala
+++ src/main/scala/net/tqft/mlp/package.scala
@@ -14,13 +14,15 @@ package object mlp {
     ISSNs.`Journal of Functional Analysis`,
     ISSNs.`Journal of Number Theory`,
     ISSNs.`Journal of Combinatorial Theory A`,
-    ISSNs.`Journal of Combinatorial Theory B`)
+    ISSNs.`Journal of Combinatorial Theory B`,
+    ISSNs.`Journal of Algebra`,
+    ISSNs.`Journal of Pure and Applied Algebra`)
 
   val selectedYears = Seq(2013)
 
   def selectedCoverage = for (j <- selectedJournals; y <- selectedYears; a <- Search.inJournalYear(j, y)) yield a
 
-  def extendedJournals = Iterator(ISSNs.`Journal of Algebra`, ISSNs.`Journal of Pure and Applied Algebra`) ++ selectedJournals
+  def extendedJournals = Iterator() ++ selectedJournals
   def extendedYears = Seq(2010, 2013)
 
   def extendedCoverage = for (j <- extendedJournals; y <- extendedYears; a <- Search.inJournalYear(j, y)) yield a
