135b88ef44fb1b4b9066388bb852617f91e3ee14
Scott Morrison <scott@tqft.net>
2014-02-09 15:25:02 +1100

    removing eclipse files

---
+++ src/test/scala/net/tqft/mathscinet/CleanBIBTEX.scala
@@ -0,0 +1,17 @@
+package net.tqft.mathscinet
+
+import net.tqft.toolkit.amazon.S3
+import scala.collection.parallel.ForkJoinTaskSupport
+import net.tqft.util.FirefoxSlurp
+
+object CleanBIBTEX extends App {
+  FirefoxSlurp.disable
+  
+  Article.disableBibtexSaving
+  
+  val pool = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(100))
+
+  for (group <- Articles.fromBibtexFile(System.getProperty("user.home") + "/projects/arxiv-toolkit/50.bib").grouped(1000); article <- { val p = group.par; p.tasksupport = pool; p }; bibtex = article.bibtex.toBIBTEXString; if bibtex.contains("&amp;") || bibtex.contains("&gt;") || bibtex.contains("&lt;")) {
+    println(bibtex)
+  }
+}
