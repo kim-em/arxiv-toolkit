d7e47f8f7d5256d2163d499607a81b6f9c374dbc
Scott Morrison <scott@tqft.net>
2014-02-14 22:20:30 +1100

    various

---
+++ src/main/scala/net/tqft/mlp/sql/package.scala
@@ -0,0 +1,5 @@
+package net.tqft.mlp
+
+package object sql {
+  type bibtexTuple = (Int, String, Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String], Option[String])
+}
