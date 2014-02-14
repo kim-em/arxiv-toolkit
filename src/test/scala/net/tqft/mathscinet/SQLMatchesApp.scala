215441f22dad454bc5ddd8eab20381ccbbd6463b
Scott Morrison <scott@tqft.net>
2014-02-14 21:36:40 +1100

    populating a table of candidate matches

--- src/test/scala/net/tqft/mathscinet/SQLMatchesApp.scala
+++ src/test/scala/net/tqft/mathscinet/SQLMatchesApp.scala
@@ -6,26 +6,36 @@ import scala.slick.driver.MySQLDriver.simple._
 import net.tqft.toolkit.Logging
 import net.tqft.util.pandoc
 import scala.collection.parallel.ForkJoinTaskSupport
-import net.tqft.citationsearch.Search.Citation
+import net.tqft.citationsearch._
 
 object SQLMatchesApp extends App {
 
   SQL { implicit session =>
-//    There's no good way to rebuild this table; if the citation search database is expanded, just clear this table and rebuild.
-//    SQLTables.arxiv_mathscinet_matches.delete
-    
-    def page(k: Int) = (for (
-      a <- SQLTables.arxiv;
-      if !SQLTables.arxiv_mathscinet_matches.filter(_.arxivid === a.arxivid).exists
-    ) yield (a.arxivid, a.title, a.authors, a.journalref.?)).drop(k * 100).take(100).iterator
+    //    There's no good way to rebuild this table; if the citation search database is expanded, just clear this table and rebuild.
+    //    SQLTables.arxiv_mathscinet_matches.delete
+
+    def page(k: Int): List[(String, String, String, Option[String])] = try {
+      (for (
+        a <- SQLTables.arxiv;
+        if a.doi.isNull;
+        if !SQLTables.arxiv_mathscinet_matches.filter(_.arxivid === a.arxivid).exists
+      ) yield (a.arxivid, a.title, a.authors, a.journalref.?)).drop(k * 100).take(100).list
+    } catch {
+      case e: Exception => {
+        Logging.warn(e)
+        page(k)
+      }
+    }
 
     val articles = Iterator.from(0).map(page).takeWhile(_.nonEmpty).flatten
-    
+
     for ((arxivid, title, authorsXML, journalref) <- articles) {
       val authors = (for (names <- (scala.xml.XML.loadString("<authors>" + authorsXML + "</authors>") \\ "author").iterator) yield (names \\ "keyname").text + ", " + (names \\ "forenames").text).mkString("", "; ", ";")
-      val results = net.tqft.citationsearch.Search.query(title + " " + authors + " " + journalref.getOrElse(""))
-      val shortResults = results.headOption.toList ++ results.tail.filter(_._2 > 0.5)
-      for ((Citation(mrnumber, _, _, _, _, _, _), score) <- shortResults) {
+      val results = net.tqft.citationsearch.Search.query(title + " " + authors + " " + journalref.getOrElse("")).results
+      for (
+        (CitationScore(Citation(mrnumber, _, _, _, _, _, _), score), i) <- results.zipWithIndex;
+        if i == 0 || score > 0.5
+      ) {
         SQLTables.arxiv_mathscinet_matches += ((arxivid, mrnumber, score))
       }
     }
