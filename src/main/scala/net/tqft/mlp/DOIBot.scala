35dfcce39a453b34f63a2277cf0261a275ab4390
Scott Morrison <scott@tqft.net>
2014-02-27 19:32:39 +1100

    checking for journals missing from the ERA list

---
+++ src/main/scala/net/tqft/mlp/DOIBot.scala
@@ -0,0 +1,33 @@
+package net.tqft.mlp
+
+import net.tqft.mathscinet.Search
+import net.tqft.journals.ISSNs
+import net.tqft.wiki.WikiMap
+import net.tqft.mathscinet.Article
+import net.tqft.util.pandoc
+import net.tqft.eigenfactor.Eigenfactor
+
+object DOIBot extends App {
+
+  lazy val doibot = {
+    val b = WikiMap("http://tqft.net/mlp/index.php")
+    b.login("doibot", "zytopex")
+    b.enableSQLReads("jdbc:mysql://mysql.tqft.net/mathematicsliteratureproject?user=readonly1&password=readonly", "mlp_")
+    b.setThrottle(10000)
+    b
+  }
+
+  //  Article.enableBibtexSaving
+
+  for (a <- extendedCoverage ++ topJournals(100)) {
+    for (link <- a.DOI.map("http://dx.doi.org/" + _).orElse(a.URL)) {
+      println("posting link for " + a.identifierString)
+      doibot("Data:" + a.identifierString + "/PublishedURL") = link
+
+    }
+  }
+
+  println("Done entering DOIs!")
+
+  net.tqft.wiki.FirefoxDriver.quit
+}
---
+++ src/main/scala/net/tqft/mlp/TitleBot.scala
@@ -0,0 +1,30 @@
+package net.tqft.mlp
+
+import net.tqft.mathscinet.Search
+import net.tqft.journals.ISSNs
+import net.tqft.wiki.WikiMap
+import net.tqft.mathscinet.Article
+import net.tqft.util.pandoc
+import net.tqft.eigenfactor.Eigenfactor
+
+object TitleBot extends App {
+
+  lazy val titlebot = {
+    val b = WikiMap("http://tqft.net/mlp/index.php")
+    b.login("titlebot", "zytopex")
+    b.enableSQLReads("jdbc:mysql://mysql.tqft.net/mathematicsliteratureproject?user=readonly1&password=readonly", "mlp_")
+    b.setThrottle(45000)
+    b
+  }
+
+  //  Article.enableBibtexSaving
+
+  for (a <- extendedCoverage ++ topJournals(100)) {
+    println("posting title for " + a.identifierString + ": " + a.title + " // " + a.wikiTitle)
+    titlebot("Data:" + a.identifierString + "/Title") = a.wikiTitle
+  }
+
+  println("Done entering titles!")
+
+  net.tqft.wiki.FirefoxDriver.quit
+}
