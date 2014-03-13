2f7119475b57018e26fa52c43a1b8645cee65fe4
Scott Morrison <scott@tqft.net>
2014-03-13 22:14:19 +1100

    more mathematicians from ANU

---
+++ build.sbt
@@ -0,0 +1,68 @@
+name := "arxiv-toolkit"
+
+organization := "net.tqft"
+
+version := "0.1.1-SNAPSHOT"
+
+scalaVersion := "2.10.3"
+
+scalacOptions += "-target:jvm-1.7"
+
+resolvers ++= Seq(
+	"Java.net Maven2 Repository" at "http://download.java.net/maven/2/",
+	"tqft.net Maven repository" at "http://tqft.net/releases",
+	"Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+	"Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases",
+	"Scala Snapshots" at "http://scala-tools.org/repo-snapshots/",
+	"simmetrics repository" at "http://maven.mse.jhu.edu/m2repository"
+)
+
+// Project dependencies
+libraryDependencies ++= Seq(
+	"net.tqft" %% "toolkit-base" % "0.1.17-SNAPSHOT",
+	"net.tqft" %% "toolkit-collections" % "0.1.17-SNAPSHOT",
+	"net.tqft" %% "toolkit-amazon" % "0.1.17-SNAPSHOT",
+	"net.tqft" %% "toolkit-wiki" % "0.1.17-SNAPSHOT",
+	"net.tqft" %% "citation-search" % "0.2-SNAPSHOT",
+	"com.ibm.icu" % "icu4j" % "51.1",
+	"rome" % "rome" % "1.0",
+	"org.apache.httpcomponents" % "httpclient" % "4.3.2",
+	"commons-io" % "commons-io" % "2.4",
+	"com.google.code.findbugs" % "jsr305" % "2.0.1",
+	"net.sourceforge.htmlunit" % "htmlunit" % "2.12",
+	"org.seleniumhq.selenium" % "selenium-support" % "2.39.0",
+	"org.seleniumhq.selenium" % "selenium-htmlunit-driver" % "2.39.0",
+	"org.seleniumhq.selenium" % "selenium-firefox-driver" % "2.39.0",
+	"org.seleniumhq.selenium" % "selenium-chrome-driver" % "2.39.0",
+	"be.roam.hue" % "hue" % "1.1",
+	"eu.medsea.mimeutil" % "mime-util" % "2.1.3",
+	"mysql" % "mysql-connector-java" % "5.1.24",
+	"com.typesafe.slick" %% "slick" % "2.0.0-RC1",
+	"uk.ac.shef.wit" % "simmetrics" % "1.6.2",
+	"org.apache.pdfbox" % "pdfbox" % "1.8.3"
+)
+
+// lift-json
+libraryDependencies ++= {
+  val liftVersion = "2.6-M2" // Put the current/latest lift version here
+  Seq(
+    "net.liftweb" %% "lift-util" % liftVersion,
+    "net.liftweb" %% "lift-json" % liftVersion,
+    "net.liftweb" %% "lift-json-ext" % liftVersion
+    )
+}
+
+// Test dependencies
+libraryDependencies ++= Seq(
+	"junit" % "junit" % "4.8" % "test",
+	"org.scalatest" % "scalatest_2.10" % "2.0" % "compile,test"
+)
+
+// Sometimes it's useful to see debugging out from the typer (e.g. to resolve slow compiles)
+// scalacOptions += "-Ytyper-debug"
+
+EclipseKeys.withSource := true
+
+publishTo := Some(Resolver.sftp("tqft.net", "tqft.net", "tqft.net/releases") as ("scottmorrison", new java.io.File("/Users/scott/.ssh/id_rsa")))
+
+net.virtualvoid.sbt.graph.Plugin.graphSettings
---
+++ src/test/scala/net/tqft/mathscinet/VerifyJournalCompleteApp.scala
@@ -0,0 +1,192 @@
+package net.tqft.mathscinet
+
+import java.io.File
+import net.tqft.journals.ISSNs
+import net.tqft.mlp.sql.SQL
+import net.tqft.mlp.sql.SQLTables
+import net.tqft.util.pandoc
+import net.tqft.journals.Journals
+import java.nio.file.Files
+import java.nio.file.DirectoryStream
+import java.nio.file.Path
+import java.nio.file.Paths
+import net.tqft.journals.Journal
+import net.tqft.toolkit.Logging
+import net.tqft.toolkit.Throttle
+
+object VerifyJournalCompleteApp extends App {
+
+  val missingButWhoCares_? = Seq("MR0337466", "MR0337465", "MR0337467", "MR1690573", "MR1690574") ++ // birthdays
+    Seq("MR0585438", "MR0585435", "MR0973821") ++ // bad data on mathscinet?
+    Seq("MR0949339", "MR1895524") ++ // memorials
+    Seq("MR0532067") // errata
+
+    val missingFromElsevier = """
+A diametric theorem for edges - Ahlswede, R. and Khachatrian, L. H. - J. Combin. Theory Ser. A 92 (2000), no. 1, 1–16 - MR1783935
+Longest paths in semicomplete multipartite digraphs - Volkmann, Lutz - Discrete Math. 199 (1999), no. 1-3, 279–284 - MR1675934
+Necessary conditions for Hamiltonian split graphs - Peemöller, Jürgen - Discrete Math. 54 (1985), no. 1, 39–47 - MR0787491
+Undecided Ramsey numbers for paths - Lindström, Bernt - Discrete Math. 43 (1983), no. 1, 111–112 - MR0680310
+On the existence of cyclic and pseudo-cyclic MDS codes - Maruta, Tatsuya - European J. Combin. 19 (1998), no. 2, 159–174 - MR1607933
+Characterization of antidominant solutions of linear differential equations - Hanschke, Thomas - J. Comput. Appl. Math. 40 (1992), no. 3, 351–361 - MR1170914
+Inclusion relationships among permutation problems - Bovet, Daniel P. and Panconesi, Alessandro - Discrete Appl. Math. 16 (1987), no. 2, 113–123 - MR0874910
+Dynamic programming of expectation and variance - Quelle, Gisbert - J. Math. Anal. Appl. 55 (1976), no. 1, 239–252 - MR0411651
+On the completeness of the system of functions \{e^{\alpha(n+a)x}\sin(n+a)x\}^\infty_{n=0} - Dostanić, M. R. - J. Math. Anal. Appl. 203 (1996), no. 3, 672–676 - MR1417122
+Vector systems of exponentials and zeroes of entire matrix-functions - Ivanov, S. A. and Pavlov, B. S. - J. Math. Anal. Appl. 74 (1980), no. 2, 25–31, 118 - MR0573400
+ABC candies - Ribenboim, Paulo - J. Number Theory 81 (2000), no. 1, 48–60 MR1743505
+Metric methods three examples and a theorem - Fitting, Melvin - J. Logic Programming 21 (1994), no. 3, 113–127 - MR1300126
+The stable models of a predicate logic program - Marek, V. Wiktor and Nerode, Anil and Remmel, Jeffrey B. - J. Logic Programming 21 (1994), no. 3, 129-154 - MR1300127
+On the power of rule-based query languages for nested data models - Vadaparty, Kumar V. - J. Logic Programming 21 (1994), no. 3, 155–175 - MR1300128
+Distributed logic programming - Brogi, Antonio and Gorrieri, Roberto - J. Logic Programming 15 (1993), no. 4, 295–335 - MR1208829
+A completeness result for SLDNF-resolution - Stroetmann, Karl - J. Logic Programming 15 (1993), no. 4, 337–355 - MR1208830
+On the existence of cyclic and pseudo-cyclic MDS codes - Maruta, Tatsuya - European J. Combin. 19 (1998), no. 2, 159–174 - MR1607933
+Model-theoretic forcing in logic with a generalized quantifier - Bruce, Kim B. - Ann. Math. Logic 13 (1978), no. 3, 225–265 - MR0491860
+Bounds on the number of fuzzy functions - Kameda, T. and Sadeh, E. - Information and Control 35 (1977), no. 2, 139–145 - MR0456965      
+""".split("\n").filter(_.nonEmpty).map(_.split(" ").last).collect({ case MRIdentifier(id) => id })
+    
+  Article.disableBibtexSaving
+
+  val source = Paths.get("/Volumes/Repository Backups/elsevier-oa/")
+  val target = Paths.get(System.getProperty("user.home") + "/Literature")
+
+  val elsevierJournals = Journals.names.filter(p => ISSNs.Elsevier.contains(p._1))
+
+  def pdfs(directory: Path) = {
+    import scala.collection.JavaConverters._
+
+    val identifierRegex = "MR[0-9]{7}".r
+
+    Files.newDirectoryStream(directory, new DirectoryStream.Filter[Path] {
+      override def accept(path: Path) = {
+        identifierRegex.findFirstMatchIn(path.getFileName.toString).nonEmpty
+      }
+    }).iterator.asScala
+  }
+
+  val completedJournals = Nil/*Seq(
+      "Discrete Optim.", 
+      "J. Approx. Theory",  
+      "Topology Appl.",
+      "Differential Geom. Appl.", 
+      "Finite Fields Appl.", 
+      "European J. Combin.", 
+      "Appl. Math. Model.",
+      "Stochastic Process. Appl.",
+      "Comput. Geom.",
+      "Math. Comput. Modelling",
+//      "J. Comput. Appl. Math.",
+      "Adv. Math.",
+      "Adv. in Appl. Math.",
+      "J. Number Theory",
+      "J. Math. Pures Appl.",
+      "J. Math. Anal. Appl.",
+      "J. Combin. Theory Ser. A",
+      "Topology",
+      "Appl. Comput. Harmon. Anal.",
+      "Indag. Math. (N.S.)",
+      "Comput. Math. Appl.",
+      "J. Approx. Reason.",
+      "J. Differential Equations",
+      "J. Complexity",
+      "Ann. Inst. H. Poincaré Anal. Non Linéaire",
+      "Bull. Sci. Math.",
+      "J. Discrete Algorithms",
+      "J. Funct. Anal.",
+      "Appl. Math. Lett.",
+      "Inform. and Control",
+      "J. Logic Programming",
+      "J. Multivariate Anal.",
+      "J. Combin. Theory Ser. B",
+      "Exposition. Math.",
+      "J. Comput. System Sci.",
+      "Math. Modelling",
+      "Historia Math.",
+      "Discrete Appl. Math.",
+      "J. Log. Algebr. Program.",
+      "General Topology Appl.",
+      "Discrete Math.",
+      "J. Appl. Log.",
+      "J. Pure Appl. Algebra",
+      "Sci. Comput. Programming",
+      "Ann. Pure Appl. Logic")*/
+  // TODO automatically assemble the .tar.gz files, as long as no articles are marked as missing above
+  // TODO automatically upload them to S3
+  
+  var journalCount = 0
+  for ((issn, name) <- scala.util.Random.shuffle(elsevierJournals.toSeq); if !completedJournals.contains(name)) {
+
+    journalCount += 1
+    println("Checking " + name + " " + issn + " (" + journalCount + "/" + elsevierJournals.size + ")")
+
+    val journalPath = target.resolve(name)
+
+    if (!Files.exists(journalPath)) {
+      Files.createDirectory(journalPath)
+      if (Files.exists(source.resolve("LICENSE.pdf"))) {
+        Files.createSymbolicLink(journalPath.resolve("LICENSE.pdf"), source.resolve("LICENSE.pdf"))
+      }
+    }
+
+    val files = scala.collection.mutable.Set[Path]() ++ pdfs(journalPath)
+
+    val throttle = Throttle.linearBackoff(10000)
+
+    var count = 0
+    val articles = Journal(issn).openAccess
+    .filterNot(a => missingButWhoCares_?.contains(a.identifierString))
+    .filterNot(a => missingFromElsevier.contains(a.identifier))
+    .toSeq
+
+    println("Total articles: " + articles.size)
+    println(articles.groupBy(_.year).mapValues(_.size).toSeq.sorted.mkString(" "))
+
+    def incr {
+      synchronized {
+        count += 1
+      }
+    }
+
+    for (group <- articles.grouped(100); article <- group.par; if article.journalOption != Some("Advancement in Math.")) {
+      try {
+        incr
+        val filename = article.constructFilename()
+        val sourceFile = source.resolve(filename)
+        if (!Files.exists(sourceFile)) {
+          println("Not found in source directory: ")
+          println(filename)
+          println(article.bibtex.toBIBTEXString)
+          article.savePDF(source.toFile())
+        }
+        if (Files.exists(sourceFile)) {
+          val targetFile = journalPath.resolve(filename)
+          if (!Files.exists(targetFile)) {
+            println("Not found in target directory: ")
+            println(filename)
+            println(article.bibtex.toBIBTEXString)
+            try {
+              println("Creating symbolic link ...")
+              Files.createSymbolicLink(targetFile, sourceFile)
+            } catch {
+              case e: Exception => {
+                Logging.error("Exception while creating symbolic link for:\n" + article.bibtex.toBIBTEXString, e)
+              }
+            }
+          } else {
+            synchronized {
+              files -= targetFile
+            }
+          }
+        }
+      } catch {
+        case e: Exception => {
+          Logging.error("Exception while checking: " + article.bibtex.toBIBTEXString, e)
+        }
+      }
+    }
+
+    println("Found " + count + " articles in " + name)
+    for (file <- files) {
+      println("Deleting: " + file.getFileName)
+      Files.delete(file)
+    }
+  }
+}
