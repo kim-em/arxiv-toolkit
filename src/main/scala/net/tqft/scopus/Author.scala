1a88fa2a62e6b3e2af689baa2db64969423664aa
Scott Morrison <scott@tqft.net>
2014-04-12 22:31:16 +1000

    actually download missing stuff

---
+++ src/main/scala/net/tqft/scopus/Author.scala
@@ -0,0 +1,31 @@
+package net.tqft.scopus
+
+import net.tqft.util.Slurp
+import net.tqft.util.FirefoxSlurp
+
+object Scopus {
+  private var latch = true
+  lazy val preload = {
+    //  use FirefoxSlurp directly, to avoid the cache on this first hit.
+    if (latch) {
+      latch = false
+      (new FirefoxSlurp {})("http://www.scopus.com/")
+    }
+    None
+  }
+}
+
+case class Author(id: Long, name: String) {
+  def URL = "http://www.scopus.com/authid/detail.url?authorId=" + id.toString
+
+  def publicationsURL = "http://www.scopus.com/search/submit/author.url?authorId=" + id.toString
+
+  lazy val publications: Stream[Article] = {
+    //    <a onclick="javascript:submitRecord('2-s2.0-70349820748','9','5');" title="Show document details" href="http://www.scopus.com/record/display.url?eid=2-s2.0-70349820748&amp;origin=resultslist&amp;sort=plf-f&amp;src=s&amp;sid=6995D3618DB9D6D6ACB543BC41D0E039.FZg2ODcJC9ArCe8WOZPvA%3a20&amp;sot=aut&amp;sdt=a&amp;sl=35&amp;s=AU-ID%28%22Morrison%2c+Scott%22+7201672329%29&amp;relpos=9&amp;relpos=9&amp;citeCnt=5&amp;searchTerm=AU-ID%28%5C%26quot%3BMorrison%2C+Scott%5C%26quot%3B+7201672329%29">Skein theory for the D2 n planar algebras</a>
+
+    val r = """<a onclick="javascript:submitRecord\('(.*)','[0-9]*','[0-9]*'\);" title="Show document details" href=".*">(.*)</a>""".r
+    (for (line <- Slurp(publicationsURL); if line.contains("Show document details"); m <- r.findAllMatchIn(line)) yield {
+      Article(m.group(1), m.group(2))
+    }).toStream
+  }
+}
---
+++ src/test/scala/net/tqft/mathscinet/SQLMatchesApp.scala
@@ -0,0 +1,44 @@
+package net.tqft.mathscinet
+
+import net.tqft.mlp.sql.SQL
+import net.tqft.mlp.sql.SQLTables
+import scala.slick.driver.MySQLDriver.simple._
+import net.tqft.toolkit.Logging
+import net.tqft.util.pandoc
+import scala.collection.parallel.ForkJoinTaskSupport
+import net.tqft.citationsearch._
+
+object SQLMatchesApp extends App {
+
+  SQL { implicit session =>
+    //    There's no good way to rebuild this table; if the citation search database is expanded, just clear this table and rebuild.
+    //    SQLTables.arxiv_mathscinet_matches.delete
+
+    def page(k: Int): List[(String, String, String, Option[String])] = try {
+      (for (
+        a <- SQLTables.arxiv;
+        if a.doi.isNull;
+        if !SQLTables.arxiv_mathscinet_matches.filter(_.arxivid === a.arxivid).exists
+      ) yield (a.arxivid, a.title, a.authors, a.journalref.?)).drop(k * 100).take(100).list
+    } catch {
+      case e: Exception => {
+        Logging.warn(e)
+        page(k)
+      }
+    }
+
+    val articles = Iterator.continually(Iterator.from(0).map(page).takeWhile(_.nonEmpty).flatten).takeWhile(_.nonEmpty).flatten
+
+    for ((arxivid, title, authorsXML, journalref) <- articles) {
+      val authors = (for (names <- (scala.xml.XML.loadString("<authors>" + authorsXML + "</authors>") \\ "author").iterator) yield (names \\ "keyname").text + ", " + (names \\ "forenames").text).mkString("", "; ", ";")
+      val results = net.tqft.citationsearch.Search.query(title + " " + authors + " " + journalref.getOrElse("")).results
+      for (
+        (CitationScore(c : Citation, score), i) <- results.zipWithIndex;
+        if i == 0 || score > 0.5;
+        if c.MRNumber.nonEmpty
+      ) {
+        SQLTables.arxiv_mathscinet_matches += ((arxivid, c.MRNumber.get, score, c.best))
+      }
+    }
+  }
+}
