a59ff31474f2910734e903cb47586e2c641c5b97
Scott Morrison <scott@tqft.net>
2014-06-12 10:59:17 +1000

    comparing citations

--- src/main/scala/net/tqft/mathscinet/Author.scala
+++ src/main/scala/net/tqft/mathscinet/Author.scala
@@ -12,8 +12,32 @@ case class Author(id: Int, name: String) {
       name.split(",").map(_.trim).filter(_.nonEmpty).toSeq match {
         case Seq(one) => one
         case Seq(one, two) => two + " " + one
-        case Seq(one, suffix @ ("Jr." | "Jr" | "Sr." | "jr." | "jun." | "I" | "II" | "III" | "IV" | "V"), two) => two + " " + one + ", " + suffix
-        case Seq("Cooper", "Randolph G.", "III", "Jr.") => "Randolph G. Cooper, III, Jr." // That doesn't even make sense. 
+        case Seq(one, suffix @ ("Jr." | "Jr" | "Sr." | "jr." | "jun." | """J{\'u}nior""" | "Junior" | "ml." | "I" | "II" | "II." | "III" | "III." | "3rd" | "3rd." | "IV" | "V" | "V." | "S.J." | "S. J." | "E. G." | "M.a."), two) => two + " " + one + ", " + suffix
+        case Seq("Vogan", "David A.", "Jr.") => "David A. Vogan, Jr."
+        case Seq("Miller", "G.", "V") => "G. Miller, V"
+        case Seq("Day", "M.", "Mahlon") => "Mahlon M. Day"
+        case Seq("Arnold", "James", "Jr.", "E.") => "James E. Arnold, Jr."
+        case Seq("Rodeja", "E. G.-", "F.") => "F. Rodeja, E. G."
+        case Seq("Cooper", "Randolph G.", "III", "Jr.") => "Randolph G. Cooper, III, Jr." // That doesn't even make sense.
+        case Seq("Young", "Gail", "Jr.", "S") => "Gail S. Young, Jr."
+        case Seq("Young", "Gail", "Jr.", "S.") => "Gail S. Young, Jr."
+        case Seq("Good", "J.", "I") => "J. Good, I"
+        case Seq("Ramamohana", "C.", "Rao") => "Rao C. Ramamohana"
+        case Seq("Dou", "Alberto", "S. I.") => "Alberto Dou, S. I."
+        case Seq("Kolos", "C. C. J.", "W. Roothaan") => "W. Roothaan Kolos, C. C. J."
+        case Seq("Snyder", "M.", "Willard") => "Willard M. Snyder"
+        case Seq("Long", "E.", "Paul") => "Paul E. Long"
+        case Seq("Best", "J.", "Michael") => "Michael J. Best"
+        case Seq("Kou", "R.", "Shauying") => "Shauying R. Kou"
+        case Seq("Besieris", "M.", "Ioannis") => "Ioannis M. Besieris"
+        case Seq("Kuiper", "H.", "Nicolaas") => "Nicolaas H. Kuiper"
+        case Seq("da Rocha", "Galdino", "Filho") => "Galdino da Rocha, Filho"
+        case Seq("Ditor", "Z.", "Seymour") => "Seymour Z. Ditor"
+        case Seq("Sankara", "K.", "Pillai") => "Pillai K. Sankara"
+        case Seq("Kazarjan", "A.", """{\`E}""") => """{\`E}. A. Kazarjan"""
+        case Seq("Cruz", "A.", "Marianito") => "Marianito A. Cruz"
+        case Seq("Keezer", "Sister", "James Miriam") => "James Miriam Keezer"  
+        case Seq("Wolf", "R.", "Thomas") => "Thomas R. Wolf"
       }
     }
 }
--- src/test/scala/net/tqft/mathscinet/SQLAuxApp.scala
+++ src/test/scala/net/tqft/mathscinet/SQLAuxApp.scala
@@ -11,6 +11,8 @@ object SQLAuxApp extends App {
 
   val pool = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(100))
 
+  val seen = scala.collection.mutable.Set[Int]()
+
   SQL { implicit session =>
     def articlesPage(k: Int) = {
       println("retrieving page " + k)
@@ -22,23 +24,24 @@ object SQLAuxApp extends App {
 
     var group = articlesPage(0)
     while (group.nonEmpty) {
-    	println(group.size)
-        for (a <- { val p = group.par; p.tasksupport = pool; p }) {
-          try {
-            val data = (a.identifier, a.textTitle, a.wikiTitle, a.authorsText, a.citation_text, a.citation_markdown, a.citation_html)
-             SQLTables.mathscinet_aux.citationData += (data)
-             println(SQLTables.mathscinet_aux.citationData.insertStatementFor(data) + ";")
-          } catch {
-            case e: com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException if e.getMessage().startsWith("Duplicate entry") => {
-              println("skipping " + a.identifierString)
-            }
-            case e: Exception => {
-              Logging.error("Exception while inserting \n" + a.bibtex.toBIBTEXString, e)
-            }
+      println(group.size)
+      for (a <- { val p = group.par; p.tasksupport = pool; p }) {
+        try {
+          val data = (a.identifier, a.textTitle, a.wikiTitle, a.authorsText, a.citation_text, a.citation_markdown, a.citation_html)
+          SQLTables.mathscinet_aux.citationData += (data)
+          println(SQLTables.mathscinet_aux.citationData.insertStatementFor(data) + ";")
+        } catch {
+          case e: com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException if e.getMessage().startsWith("Duplicate entry") => {
+            println("skipping " + a.identifierString)
+          }
+          case e: Exception => {
+            Logging.error("Exception while inserting \n" + a.bibtex.toBIBTEXString, e)
+          }
         }
       }
+      seen ++= group.map(_.identifier)
       group = articlesPage(0)
-
+      group = group.filterNot(a => seen.contains(a.identifier))
     }
   }
 }
