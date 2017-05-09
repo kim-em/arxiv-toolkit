f3bae3c61356f0780df0ce9dbc7722422c407152
Scott Morrison <scott@tqft.net>
2017-05-09 22:32:26 +1000

    looking at extracting citations from arxiv papers again

--- src/test/scala/net/tqft/arxiv2/SourcesApp.scala
+++ src/test/scala/net/tqft/arxiv2/SourcesApp.scala
@@ -4,13 +4,21 @@ import net.tqft.citationsearch.CitationScore
 import net.tqft.util.pandoc
 
 object SourcesApp extends App {
-  for ((key, text, o) <- Sources.referencesResolved("0705.0069")) {
-    println(pandoc.latexToText(text))
-    o.map({
-      case CitationScore(citation, score) =>
-        println(citation.best)
-        println(citation.title + " - " + citation.authors + " - " + citation.citation_text)
-    })
-    println()
+//  for ((key, text, o) <- Sources.referencesResolved("math/9912028")) {
+//    println(pandoc.latexToText(text))
+//    o.map({
+//      case CitationScore(citation, score) =>
+//        println(citation.best)
+//        println(citation.title + " - " + citation.authors + " - " + citation.citation_text)
+//    })
+//    println()
+//  }
+  for(s <- Sources.bibitems("math/9912028")) {
+    println(s)
+    println
+  }
+  for(s <- Sources.bibitems("1701.00567")) {
+    println(s)
+    println
   }
 }
